b9e8da5f7f43d7f5c94a75a4727d9a4aab348fa8
Mahathir Almashor <malmash@au1.ibm.com>
2016-11-24 23:19:05 +1100

    hah! figured it out. I was changing the array as I was recursing. Gorram it... always sketch it out on paper (and a shower and some rest helped).
    Signed-off-by: Mahathir Almashor <malmash@au1.ibm.com>

--- src/com/pany/two/Two.java
+++ src/com/pany/two/Two.java
@@ -24,7 +24,7 @@ public class Two extends Runner
     {
         this.result = new ArrayList<String>();
 
-        Integer[] arrSub = {8,9,7};
+        Integer[] arrSub = {8,9,7,7};
         this.original = new ArrayList<Integer>(Arrays.asList(arrSub));
     }
 
@@ -33,30 +33,38 @@ public class Two extends Runner
                          ArrayList<Integer> left,
                          ArrayList<Integer> right)
     {
-        section("" + right.toArray().length);
-        log("Bef = " + left.toString() + " | " + right.toString());
+//        section("" + right.toArray().length);
+        section(left.toString() + " " + right.toString());
 
         if(right.isEmpty())
         {
             log("RESULT += " + left.toString());
             result.add(left.toString());
+            log("END END END END");
             return;
         }
 
-        for (int idx = 0; idx < right.toArray().length ; idx++)
-        {
-            left.add(right.remove(idx));
-            lsl("Aft = ");
-            lsl(left.toString());
-            lsl(" | ");
-            lsl(right.toString());
-            lsl(", foridx=");
-            lsl(idx);
+        ArrayList<Integer> curLeft = new ArrayList<Integer>(left);
+        ArrayList<Integer> curRight = new ArrayList<Integer>(right);
 
+        int idx = 0;
+        for (; idx < right.toArray().length ; idx++)
+        {
+            section(idx + " " + left.toString() + " " + right.toString());
+            left.add(right.remove(idx));
+            log("recurse in");
             recurse(result,
                     new ArrayList<Integer>(left),
                     new ArrayList<Integer>(right));
+            log("recurse out");
+            space();
+
+            left = new ArrayList<Integer>(curLeft);
+            right = new ArrayList<Integer>(curRight);
         }
+
+        log("end func. idx was:" + idx);
+        space();
     }
 
     @Override
@@ -84,6 +92,7 @@ public class Two extends Runner
         {
             log(s);
         }
+        log("Number of Permutations: " + this.result.toArray().length);
 
     }
 }
